c31e0d60bb9462503f29f30a9bdc08f58b82ef77
Maxim <mbzozn@mail.ru>
2021-11-09 11:32:00 +0300

    Done

---
+++ OOP005/OOP005/Fractus.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP005
+{
+    class Fractus
+    {
+        public readonly bool CreatusEst = false;
+
+        readonly int _numerator; 
+        readonly int _denominator;
+        readonly double _douNume;
+        readonly string _printer;        
+
+        public Fractus(int numerator, int denominator)
+        {
+            if( denominator == 0 )
+            {
+                Console.WriteLine("Divide by Zerou");
+                throw new DivideByZeroException();
+            }
+
+            _numerator = numerator;
+            _denominator = denominator;
+
+            _douNume = _numerator / _denominator;
+
+            _printer = $"{_numerator}/{_denominator}";
+
+            CreatusEst = true;
+        }
+
+        public static Fractus operator ++(Fractus primis) => LibFractus.Do(primis, 1, 0);
+        public static Fractus operator --(Fractus primis) => LibFractus.Do(primis, 1, 1);
+        public static Fractus operator +(Fractus primis, Fractus secundus) => LibFractus.Do(primis, secundus, 0);
+        public static Fractus operator -(Fractus primis, Fractus secundus) => LibFractus.Do(primis, secundus, 1);
+        public static Fractus operator *(Fractus primis, Fractus secundus) => LibFractus.Do(primis, secundus, 2);
+        public static Fractus operator /(Fractus primis, Fractus secundus) => LibFractus.Do(primis, secundus, 3);
+
+        public static bool operator ==(Fractus primis, Fractus secundus) => LibFractus.Comparator(primis, secundus, 0);        
+        public static bool operator !=(Fractus primis, Fractus secundus) => !LibFractus.Comparator(primis, secundus, 0);
+        public static bool operator >(Fractus primis, Fractus secundus) => LibFractus.Comparator(primis, secundus, 1);
+        public static bool operator <(Fractus primis, Fractus secundus) => LibFractus.Comparator(primis, secundus, 2);
+        public static bool operator >=(Fractus primis, Fractus secundus) => !LibFractus.Comparator(primis, secundus, 2);
+        public static bool operator <=(Fractus primis, Fractus secundus) => !LibFractus.Comparator(primis, secundus, 1);
+
+        public static Fractus operator +(Fractus primis, int value) => LibFractus.Do(primis, value, 0);
+        public static Fractus operator -(Fractus primis, int value) => LibFractus.Do(primis, value, 1);
+        public static Fractus operator *(Fractus primis, int value) => LibFractus.Do(primis, value, 2);
+        public static Fractus operator /(Fractus primis, int value) => LibFractus.Do(primis, value, 3);
+
+        public static bool operator ==(Fractus primis, int value) => LibFractus.Comparator(primis, value, 0);
+        public static bool operator !=(Fractus primis, int value) => !LibFractus.Comparator(primis, value, 0);
+        public static bool operator >(Fractus primis, int value) => LibFractus.Comparator(primis, value, 1);
+        public static bool operator <(Fractus primis, int value) => LibFractus.Comparator(primis, value, 2);
+        public static bool operator >=(Fractus primis, int value) => !LibFractus.Comparator(primis, value, 2);
+        public static bool operator <=(Fractus primis, int value) => !LibFractus.Comparator(primis, value, 1);
+
+        public int Numerator { get { return _numerator; } }
+        public int Denominator { get { return _denominator; } }
+        public double GetDou { get { return _douNume; } }
+
+        public void Print()
+        {
+            Console.WriteLine(_printer);
+        }
+
+        public override string ToString()
+        {
+            return _printer;
+        }
+    }   
+}
---
+++ OOP005/OOP005/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace OOP005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Execute();
+        }
+
+        static void Execute()
+        {
+            int SomeValue = 3;
+            Fractus First = new Fractus(1, 3);
+            Fractus Second = new Fractus(7, 13);
+            Fractus Tertius = new Fractus(11, 31);
+
+            Console.WriteLine($"Increment = {++First}");
+            Console.WriteLine($"Decrement = {--Second}");
+
+            Console.WriteLine($"Summ {First} + {Second} = {First + Second}");
+            Console.WriteLine($"Summ {First} - {Second} = {First - Second}");
+            Console.WriteLine($"Summ {First} * {Second} = {First * Second}");
+            Console.WriteLine($"Summ {First} / {Second} = {First / Second}");
+
+            Console.WriteLine($"Summ {First} == {Second} = {First == Second}");
+            Console.WriteLine($"Summ {First} != {Second} = {First != Second}");
+            Console.WriteLine($"Summ {First} > {Second} = {First > Second}");
+            Console.WriteLine($"Summ {First} < {Second} = {First < Second}");
+            Console.WriteLine($"Summ {First} >= {Second} = {First >= Second}");
+            Console.WriteLine($"Summ {First} <= {Second} = {First <= Second}");
+
+
+            Console.WriteLine($"Summ {First} + {SomeValue} = {First + SomeValue}");
+            Console.WriteLine($"Summ {First} - {SomeValue} = {First - SomeValue}");
+            Console.WriteLine($"Summ {First} * {SomeValue} = {First * SomeValue}");
+            Console.WriteLine($"Summ {First} / {SomeValue} = {First / SomeValue}");
+
+            Console.WriteLine($"Summ {First} == {SomeValue} = {First == SomeValue}");
+            Console.WriteLine($"Summ {First} != {SomeValue} = {First != SomeValue}");
+            Console.WriteLine($"Summ {First} > {SomeValue} = {First > SomeValue}");
+            Console.WriteLine($"Summ {First} < {SomeValue} = {First < SomeValue}");
+            Console.WriteLine($"Summ {First} >= {SomeValue} = {First >= SomeValue}");
+            Console.WriteLine($"Summ {First} <= {SomeValue} = {First <= SomeValue}");
+
+            Console.WriteLine($"{First} + {Second} / {Tertius} - {SomeValue} = {First + Second / Tertius - SomeValue} ");
+           
+            Console.ReadKey();
+        }
+    }
+}
