18e225c4695ee3ca1cc1fe5176b7b695c65ed21e
Maxim <mbzozn@mail.ru>
2021-11-16 12:10:41 +0300

    Done

---
+++ OOP7/OOP7/ICodar.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP7
+{
+    interface ICodar
+    {
+        public string Encode(string someString);
+        public string Decode(string someString);
+    }
+}
---
+++ OOP7/OOP7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OOP7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Tempo = "формировании новых предложений 1924 in Monolith Studious";
+            //string Tempo = "489156 18961 4564 8";
+            ICodar CodarA = new ACoder();
+            string Execute = CodarA.Encode(Tempo);
+
+            Console.WriteLine($"Input: {Tempo}\nEncoded: {Execute}");
+            Console.WriteLine($"Decoded: {CodarA.Decode(Execute)}");
+            Console.WriteLine();
+
+
+
+            ICodar CodarB = new BCoder();
+            Execute = CodarB.Encode(Tempo);
+
+            Console.WriteLine($"Input: {Tempo}\nEncoded: {Execute}");
+            Console.WriteLine($"Decoded: {CodarB.Decode(Execute)}");
+            Console.WriteLine();
+
+            Console.WriteLine("Pres any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
